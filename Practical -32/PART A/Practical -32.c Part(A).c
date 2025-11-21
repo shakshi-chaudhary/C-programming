2e627cb9b6adeefa4a6e2bc1007211a9e68e323e
shakshi-chaudhary <shakshichaudhary.ce@gmail.com>
2025-11-21 15:05:52 +0530

    Add files via upload

---
+++ Practical -32/PART A/Practical -32.c Part(A).c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    FILE *fp;
+    char ch;
+
+    fp=fopen("Demo.txt","r");
+    if(fp==NULL)
+    {
+        printf("File does not exists!\n ");
+    }
+    else
+    {
+        fseek(fp,-1,2);
+        int size=ftell(fp)+1;
+        while(size)
+        {
+            ch=getc(fp);
+            printf("%c",ch);
+            fseek(fp,-2,1);
+            size--;
+        }
+    }
+    fclose(fp);
+    return 0;
+}
